15d1c8f6842d3d6910dba06bae86777995a77679
Jeongyounghyeon <jcs5150@naver.com>
2023-08-01 18:46:58 +0900

    setting common type

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: youjeong <youjeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 17:50:50 by youjeong          #+#    #+#             */
-/*   Updated: 2023/08/01 18:15:16 by youjeong         ###   ########.fr       */
+/*   Updated: 2023/08/01 20:02:08 by youjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,5 @@ int	main(int argc, char *args[], char **environ)
 	(void)argc;
 	(void)args;
 	(void)environ;
-	args = 0;
 	return (0);
 }
