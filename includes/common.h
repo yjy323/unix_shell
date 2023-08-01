53ce2e6fe9a711f1f47d70f7459537369afede8f
Jeongyounghyeon <jcs5150@naver.com>
2023-08-01 18:46:58 +0900

    setting common type

--- includes/common.h
+++ includes/common.h
@@ -13,4 +13,28 @@
 #ifndef COMMON_H
 # define COMMON_H
 
+typedef enum e_command_type { cm_simple, cm_connection }t_command_type;
+
+typedef struct s_word_list {
+	struct s_word_list	*next;
+	char				*word;
+}t_word_list;
+
+typedef struct s_redirect {
+	char	*word;
+	char	*filename;
+}t_redirect;
+
+typedef struct s_simple_com {
+	t_word_list	*words;
+	t_redirect	*redirect;		
+}t_simple_com;
+
+typedef struct s_command {
+	t_command_type		type;
+	t_simple_com		*simple;
+	struct s_command	*first;
+	struct s_command	*second;
+}t_command;
+
 #endif
