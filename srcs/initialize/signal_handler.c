9c8d2487b8c5274a579aa3c8a2befba9957a9b5e
Jeongyounghyeon <snorlax1106@naver.com>
2023-08-26 19:52:16 +0900

    add signal_hander for heredoc

--- srcs/initialize/signal_handler.c
+++ srcs/initialize/signal_handler.c
@@ -6,14 +6,16 @@
 /*   By: youjeong <youjeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 21:00:37 by youjeong          #+#    #+#             */
-/*   Updated: 2023/08/25 21:04:29 by youjeong         ###   ########.fr       */
+/*   Updated: 2023/08/26 19:49:58 by youjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 
 void	sigint_sighandler(int signo);
+void	sigint_sighandler_heredoc(int signo);
 
 void	sigint_sighandler(int signo)
 {
@@ -22,4 +24,10 @@ void	sigint_sighandler(int signo)
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
+}
+
+void	sigint_sighandler_heredoc(int signo)
+{
+	(void)signo;
+	exit(1);
 }
