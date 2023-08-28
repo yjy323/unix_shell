fa4480ada6a238dac999bb6dd2c1718b2c72dc0e
JooYoung <jy_23@JooYoung.local>
2023-08-28 14:24:14 +0900

    fix echo algo

--- srcs/builtin/ft_echo.c
+++ srcs/builtin/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: jy_23 <jy_23@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 19:44:20 by jy_23             #+#    #+#             */
-/*   Updated: 2023/08/27 23:18:57 by jy_23            ###   ########.fr       */
+/*   Updated: 2023/08/28 14:23:40 by jy_23            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,59 +17,53 @@
 #include "hashlib.h"
 #include "variables.h"
 
-int			ft_echo(t_word_list *list);
-static bool	is_option(char *word);
-static void	ft_echo_option(t_word_list *list, int *p_no_newline);
+int					ft_echo(t_word_list *list);
+static t_word_list	*set_option(t_word_list *list, int *p_no_newline);
+static void			print_all(t_word_list *list);
 
 int	ft_echo(t_word_list *list)
 {
-	int		status;
 	int		no_newline;
-	int		option_on;
 
 	no_newline = 0;
-	option_on = 1;
-	status = 0;
-	while (list && list->word && list->word->word)
-	{
-		if (option_on && is_option(list->word->word))
-			ft_echo_option(list, &no_newline);
-		else
-		{
-			printf("%s", list->word->word);
-			if (list->next)
-				printf(" ");
-			option_on = 0;
-		}
-		list = list->next;
-	}
+	list = set_option(list, &no_newline);
+	print_all(list);
 	if (no_newline == 0)
 		printf("\n");
-	return (status);
+	return (0);
 }
 
-static bool	is_option(char *word)
-{
-	if (*word != '-')
-		return (false);
-	while (*word++)
-	{
-		if (*word != 'n' && *word != 0)
-			return (false);
-	}
-	return (true);
-}
-
-static void	ft_echo_option(t_word_list *list, int *p_no_newline)
+static t_word_list	*set_option(t_word_list *list, int *p_no_newline)
 {
 	char	*word;
 
-	word = list->word->word;
-	while (*(++word))
+	while (list)
 	{
-		if (*word == 'n')
-			*p_no_newline = 1;
-		else
-			return ;
+		word = list->word->word;
+		if (*word != '-')
+			return (list);
+		while (*(++word))
+		{
+			if (*word == 'n')
+				*p_no_newline = 1;
+			else
+			{
+				*p_no_newline = 0;
+				break ;
+			}
+		}
+		list = list->next;
+	}
+	return (list);
+}
+
+static void	print_all(t_word_list *list)
+{
+	while (list)
+	{
+		printf("%s", list->word->word);
+		if (list->next)
+			printf(" ");
+		list = list->next;
 	}
 }
